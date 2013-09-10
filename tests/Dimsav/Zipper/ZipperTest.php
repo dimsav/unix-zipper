4bf69b03c1d5f13a5212459c3c615a3c4cedc83a
Dimitrios Savvopoulos <ds@dimsav.com>
2013-09-11 01:03:18 +0200

    Added tests for extracted files.

--- tests/Dimsav/Zipper/ZipperTest.php
+++ tests/Dimsav/Zipper/ZipperTest.php
@@ -4,20 +4,23 @@ use Dimsav\UnixZipper;
 
 class ZipperTest extends PHPUnit_Framework_TestCase {
 
-    private $destinationFile = '';
+    private $destinationFile;
+    private $extractDir;
     /** @var  UnixZipper */
     private $zipper;
 
     public function setUp()
     {
-        $this->zipper = new UnixZipper();
+        $this->zipper          = new UnixZipper();
+        $this->extractDir      = __DIR__.'/../../temp';
         $this->destinationFile = __DIR__.'/../../temp/test.zip';
-        if (file_exists($this->destinationFile)) unlink($this->destinationFile);
+
+        if (is_dir($this->extractDir)) exec('rm -rf '.realpath($this->extractDir));
     }
 
     public function testSettingArguments()
     {
-        $dir    = __DIR__.'/../../samples';
+        $dir = __DIR__.'/../../samples';
         $this->zipper->add($dir);
         $this->assertEquals($this->zipper->getFiles(), array(realpath($dir)));
 
@@ -32,15 +35,24 @@ class ZipperTest extends PHPUnit_Framework_TestCase {
         $this->zipper->setDestination($this->destinationFile);
         $this->zipper->compress();
         $this->assertFileExists($this->destinationFile);
+
+        $this->extract();
+        $this->assertFileExists($this->extractDir.'/samples/files/logs/log.txt');
+        $this->assertFileExists($this->extractDir.'/samples/files/sample.png');
     }
 
     public function testZipMultipleSources()
     {
         $this->zipper->add(__DIR__.'/../../samples');
-        $this->zipper->add(__DIR__.'/../../Dimsav');
+        $this->zipper->add(__DIR__.'/../../../src/Dimsav');
         $this->zipper->setDestination($this->destinationFile);
         $this->zipper->compress();
         $this->assertFileExists($this->destinationFile);
+
+        $this->extract();
+        $this->assertFileExists($this->extractDir.'/samples/files/logs/log.txt');
+        $this->assertFileExists($this->extractDir.'/samples/files/sample.png');
+        $this->assertFileExists($this->extractDir.'/src/Dimsav/UnixZipper/UnixZipper.php');
     }
 
     public function testExcludeFile()
@@ -49,9 +61,12 @@ class ZipperTest extends PHPUnit_Framework_TestCase {
 
         $this->zipper->exclude(__DIR__.'/../../samples/files/logs/log.txt');
         $this->zipper->setDestination(__DIR__.'/../../temp/test.zip');
-
         $this->zipper->compress();
         $this->assertFileExists(__DIR__.'/../../temp/test.zip');
+
+        $this->extract();
+        $this->assertFileExists($this->extractDir.'/samples/files/sample.png');
+        $this->assertFileNotExists($this->extractDir.'/samples/files/logs/log.txt');
     }
 
     public function testZipParentDirectoryWithPassword()
@@ -69,6 +84,18 @@ class ZipperTest extends PHPUnit_Framework_TestCase {
         $this->zipper->setDestination($this->destinationFile);
         $this->zipper->compress();
         $this->assertFileExists($this->destinationFile);
+
+        $this->extract();
+        $this->assertFileExists($this->extractDir.'/src/Dimsav/UnixZipper/UnixZipper.php');
+    }
+
+    private function extract()
+    {
+        $zip = new ZipArchive;
+        if ($zip->open($this->destinationFile) === true) {
+            $zip->extractTo($this->extractDir);
+            $zip->close();
+        }
     }
 
 }
